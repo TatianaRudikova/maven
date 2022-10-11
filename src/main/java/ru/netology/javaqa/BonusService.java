2c8826d53b672226257cbec253069d35a66dbeb6
Tatiana Rudikova <tatjana_tv@list.ru>
2022-10-11 19:01:55 +0300

    reformat code

--- src/main/java/ru/netology/javaqa/BonusService.java
+++ src/main/java/ru/netology/javaqa/BonusService.java
@@ -3,7 +3,7 @@ package ru.netology.javaqa;
 public class BonusService {
     public int calculate(int amount, boolean registered) {
         int percent = registered ? 3 : 1;
-        int bonus = amount * percent /100;
+        int bonus = amount * percent / 100;
         if (bonus > 500) {
             bonus = 500;
         }
